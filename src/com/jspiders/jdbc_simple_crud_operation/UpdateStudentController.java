6636a5fcc64def4fd6923debf01d462ec357876e
Mukesh-Kumar6070 <mukeshkumarkbsebp@gmail.com>
2023-07-23 23:12:06 +0530

    jdbc-project

---
+++ src/com/jspiders/jdbc_simple_crud_operation/UpdateStudentController.java
@@ -0,0 +1,5 @@
+package com.jspiders.jdbc_simple_crud_operation;
+
+public class UpdateStudentController {
+
+}
---
+++ src/com/jspiders/jdbc_simple_crud_operation/studentTheInsertController.java
@@ -0,0 +1,9 @@
+package com.jspiders.jdbc_simple_crud_operation;
+
+public class studentTheInsertController {
+	public static void main(String[]ars) {
+		//step - 1
+		}
+	}
+
+}
